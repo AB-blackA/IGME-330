b0b65ae540b040bdb088e02691ca858276bf14e3
AB-blackA <120144765+AB-blackA@users.noreply.github.com>
2024-02-05 10:52:55 -0500

    comments to util.js

--- HW1/black-a-pe01/src/utils.js
+++ HW1/black-a-pe01/src/utils.js
@@ -1,3 +1,8 @@
+/* Method: randomElement
+ * Params: array (any array)
+ * Purpose: return a random index of the array based on its size
+ */
+
 export function randomElement(array) { 
     
     return Math.floor(Math.random() * array.length);

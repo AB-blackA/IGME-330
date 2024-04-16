269cd763276be7a9b1e7ecc8f8cf8314732ad5f0
Andrew <blackandrew@mail.com>
2024-04-16 10:44:19 -0400

    removed node modules

---
+++ amiibo-app/src/ajax.js
@@ -0,0 +1,6 @@
+const loadXHR = (url, callback) => {
+    // set up the connection
+    // when the data loads, invoke the callback function and pass it the `xhr` object
+  };
+
+  export {loadXHR};
